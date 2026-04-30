28c3dff9b6ead4583cd4920fd7866597599713d4
Michał <m.majewski.dm@gmail.com>
2026-04-30 19:45:10 +0200

    feat: fetch products from API and render ProductList

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <main>Hello</main>
+import { getProducts } from '@/services/api'
+import { ProductList } from '@/components/ProductList/ProductList'
+
+export default async function HomePage() {
+    const data = await getProducts()
+    return (
+        <main>
+            <ProductList products={data.products}/>
+        </main>
+    )
 }
